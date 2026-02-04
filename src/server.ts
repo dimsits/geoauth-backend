8860a1c7cd6c787f024072e2e412a137b1d396d1
dimsits <sethdemeterio47@gmail.com>
2026-02-04 11:53:26 +0800

    chore: implmented .env selection based on where app is ran

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
+import dotenv from "dotenv";
+
+// Load correct env file BEFORE anything else
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+
+dotenv.config({ path: envFile });
+
 import app from "./app";
 
 const PORT = process.env.PORT || 3000;
